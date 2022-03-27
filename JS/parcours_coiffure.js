fe0ebc0b76eec63ec8bfa79e6279b5c861f532cd
Laurent Jouron <jouronlaurent@hotmail.com>
2022-03-27 19:44:25 +0200

    commit 27/03/2022

--- JS/parcours_coiffure.js
+++ JS/parcours_coiffure.js
@@ -1,13 +1,7 @@
-
-
-
-
 // Navigation bar color change
-
 let nav = document.getElementById('navigation');
 let liens = document.getElementsByClassName('liensNav');
 let icon = document.getElementById('icon');
-
 
 // onload pour commencer et onresize pour dynamique
 window.onresize = colorHeader;
@@ -17,64 +11,43 @@ function colorHeader() {
     let myWidth = window.innerWidth;
 
     // myWidth dynamique
-
     window.onscroll = function () {
-
         if (myWidth > 768) {
-
-
             if (document.documentElement.scrollTop > 980) {
-
-                nav.style.background = "#373737";
-                liens[0].style.color = "white";
-                liens[1].style.color = "white";
-                liens[2].style.color = "white";
-                liens[3].style.color = "white";
-
+                nav.style.background = "#333333";
+                liens[0].style.color = "#f1f1f1";
+                liens[1].style.color = "#f1f1f1";
+                liens[2].style.color = "#f1f1f1";
+                liens[3].style.color = "#f1f1f1";
             } else {
-                nav.style.background = "#F0EAD6";
-                liens[0].style.color = "#373737";
-                liens[1].style.color = "#373737";
-                liens[2].style.color = "#373737";
-                liens[3].style.color = "#373737";
+                nav.style.background = "#333333";
+                liens[0].style.color = "#f1f1f1";
+                liens[1].style.color = "#f1f1f1";
+                liens[2].style.color = "#f1f1f1";
+                liens[3].style.color = "#f1f1f1";
             }
-
             // second else pour mettre la nav en clair et bloqué l'anim du dessus
-
         } else {
-            nav.style.background = "#F0EAD6";
-            liens[0].style.color = "#373737";
-            liens[1].style.color = "#373737";
-            liens[2].style.color = "#373737";
-            liens[3].style.color = "#373737";
+            nav.style.background = "#333333";
+            liens[0].style.color = "#f1f1f1";
+            liens[1].style.color = "#f1f1f1";
+            liens[2].style.color = "#f1f1f1";
+            liens[3].style.color = "#f1f1f1";
         }
     }
-
-
-
-
 };
 
-
-
 // Responsiv nav
-
 function toggleNav() {
-
     if (nav.className === "") {
         nav.className += "responsive";
-
     } else {
         nav.className = "";
-
     }
 }
 
-
-
 $('html').click(function () {
     if (nav.className += "responsive") {
-
         nav.className = "";
     }
 });
@@ -82,20 +55,13 @@ $('#icon').click(function (event) {
     event.stopPropagation(); // stop la propagation, donc l'animation audessus
 });
 
-
-
-
-
-
 // Navigation Jquery, nav et arrow
-
 $("#arrow").click(function () {
     $('html,body').animate({
         scrollTop: $("#section-about").offset().top - 50 // moins 50 pour que la pointe de clip path ne rentre pas dans les titres 
     },
         'slow');
 });
-
 
 $(".liensNav:nth-child(1)").click(function () {
     $('html,body').animate({
@@ -111,7 +77,6 @@ $(".liensNav:nth-child(2)").click(function () {
         'slow');
 });
 
-
 $(".liensNav:nth-child(3)").click(function () {
     $('html,body').animate({
         scrollTop: $("#tarifs").offset().top - 50
@@ -126,7 +91,3 @@ $(".liensNav:nth-child(4)").click(function () {
     },
         'slow');
 });
-
-
-
-
